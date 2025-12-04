50ce27d8d5aaaad9e50c3e3f684a4e8f56cd1bec
school <school@works-MacBook-Air.local>
2025-12-05 00:37:02 +0800

    1

---
+++ get_next_line.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wabin-wa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/04 10:27:06 by wabin-wa          #+#    #+#             */
+/*   Updated: 2025/12/05 00:31:50 by your42login      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+char	*ft_strjoin_free(char *text, char *buff)
+{
+	char	*str;
+
+	str = ft_strjoin(text, buff);
+	free(text);
+	return (str);
+}
+
+char	*read_first_line(char *text, int fd)
+{
+	char	*buff;
+	int		bytes_read;
+
+	text = init_text(text);
+	if (text == 0)
+		return (0);
+	buff = malloc(BUFFER_SIZE + 1);
+	if (buff == 0 && (free(text), 1))
+		return (0);
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		if (ft_strchr(text, '\n'))
+			break ;
+		bytes_read = read(fd, buff, BUFFER_SIZE);
+		if (bytes_read == -1 && (free(text), free(buff), 1))
+			return (0);
+		buff[bytes_read] = 0;
+		text = ft_strjoin_free(text, buff);
+		if (text == 0 && (free(buff), 1))
+			return (0);
+	}
+	free(buff);
+	return (text);
+}
+
+char	*get_line(char *text)
+{
+	int		i;
+	char	*output_text;
+
+	if (text[0] == 0)
+		return (0);
+	i = 0;
+	while (text[i] && text[i] != '\n')
+		i++;
+	output_text = malloc(i + 2);
+	if (output_text == 0)
+		return (0);
+	output_text[0] = 0;
+	ft_strlcat(output_text, text, i + 2);
+	return (output_text);
+}
+
+char	*clean_line(char *text)
+{
+	char	*cleaned_line;
+	int		i;
+
+	if (text[0] == 0)
+	{
+		free(text);
+		return (0);
+	}
+	i = 0;
+	while (text[i] && text[i] != '\n')
+		i++;
+	if (text[i] == 0 && (free(text), 1))
+		return (0);
+	cleaned_line = malloc(ft_strlen(text) - i + 1);
+	if (cleaned_line == 0 && (free(text), 1))
+		return (0);
+	cleaned_line[0] = 0;
+	ft_strlcat(cleaned_line, text + i + 1, ft_strlen(text) - i + 1);
+	free(text);
+	return (cleaned_line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*text;
+	char		*output_text;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	text = read_first_line(text, fd);
+	if (text == 0)
+		return (0);
+	output_text = get_line(text);
+	text = clean_line(text);
+	return (output_text);
+}
+/*
+int main()
+{
+	int fd = open("text.txt", O_RDONLY);
+	char *str;
+
+	while ((str = get_next_line(fd)))
+	{
+		printf("%s",str);
+		free(str);
+	}
+
+}
+*/
---
+++ get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wabin-wa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/30 12:24:41 by wabin-wa          #+#    #+#             */
+/*   Updated: 2025/12/04 20:52:43 by wabin-wa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+# ifndef MAX_FD
+#  define MAX_FD 100
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*init_text(char *text);
+char	*read_first_line(char *text, int fd);
+char	*get_line(char *text);
+char	*clean_line(char *text);
+char	*ft_strjoin_free(char *text, char *buff);
+#endif

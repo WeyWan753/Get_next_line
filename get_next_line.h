00555432e546dcb2d9ecf01692a9086d1e885f51
Wan Mohammad Syahmi Bin Wan Mohammad Azman <wabin-wa@u80z06s02.42kl.edu.my>
2025-12-04 20:23:22 +0800

    1

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: wabin-wa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 12:24:41 by wabin-wa          #+#    #+#             */
-/*   Updated: 2025/12/04 19:48:03 by wabin-wa         ###   ########.fr       */
+/*   Updated: 2025/12/04 20:23:01 by wabin-wa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
@@ -23,5 +26,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
 char	*init_text(char *text);
-
+char	*read_first_line(char *text, int fd);
+char	*get_line(char *text);
+char	*clean_line(char *text);
+char	*ft_strjoin_free(char *text, char *buff);
 #endif
